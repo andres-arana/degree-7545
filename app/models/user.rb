4a436f106f69b903792532396aeee2994a24a67f
Andres Arana <and2arana@gmail.com>
2013-11-21 20:59:51 -0300

    Removes email confirmation when creating account from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,18 @@ class User < ActiveRecord::Base
     # First try to fetch the user based on the auth information
     user = User.where(provider: auth.provider, uid: auth.uid).first
     user ||= User.where(email: auth.info.email).first
-    user ||= User.new(
+    user ||= create_from_facebook(auth)
+    user
+  end
+
+  def self.create_from_facebook(auth)
+    user = User.new(
       name: auth.extra.raw_info.name,
       provider: auth.provider,
       uid: auth.uid,
       email: auth.info.email,
       password: Devise.friendly_token[0, 20])
+    user.skip_confirmation!
     user.save!
     user
   end
